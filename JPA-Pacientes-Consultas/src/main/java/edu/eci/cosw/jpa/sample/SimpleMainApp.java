abf53ecdc85b34925cc9f5fad1e5793f32d6470f
Juan Pablo Arevalo <juan.arevalo.merchan@hotmail.com>
2018-02-14 13:48:00 -0500

    Lab 4

--- JPA-Pacientes-Consultas/src/main/java/edu/eci/cosw/jpa/sample/SimpleMainApp.java
+++ JPA-Pacientes-Consultas/src/main/java/edu/eci/cosw/jpa/sample/SimpleMainApp.java
@@ -16,6 +16,12 @@
  */
 package edu.eci.cosw.jpa.sample;
 
+import edu.eci.cosw.jpa.sample.model.Consulta;
+import edu.eci.cosw.jpa.sample.model.Paciente;
+import edu.eci.cosw.jpa.sample.model.PacienteId;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -28,13 +34,32 @@ import org.hibernate.service.ServiceRegistry;
  * @author hcadavid
  */
 public class SimpleMainApp {
-   
-    public static void main(String a[]){
-        SessionFactory sf=getSessionFactory();
-        Session s=sf.openSession();
-        Transaction tx=s.beginTransaction();
-        
-        tx.commit();    
+
+    public static void main(String a[]) {
+        SessionFactory sf = getSessionFactory();
+        Session s = sf.openSession();
+        Transaction tx = s.beginTransaction();
+
+        // Consultando paciente
+        PacienteId id1 = new PacienteId(1, "cc");
+        Paciente query1 = (Paciente) s.load(Paciente.class, id1);
+        System.out.println("ID: " + query1.getId().getId());
+        System.out.println("Tipo ID: " + query1.getId().getTipoId());
+        System.out.println("Nombre: " + query1.getNombre());
+        System.out.println("Fecha Nacimiento: " + query1.getFechaNacimiento());
+        Set<Consulta> consultas = query1.getConsultas();
+        Iterator<Consulta> it = consultas.iterator();
+        while (it.hasNext()) {
+            System.out.println("Resumen: "+it.next().getResumen());
+            
+        }
+        // Adicionando consulta al paciente consultado
+        Date today = new Date();
+        Consulta consulta1 = new Consulta(today,query1.getNombre());
+        query1.getConsultas().add(consulta1);
+        s.saveOrUpdate("PACIENTES", query1);
+
+        tx.commit();
         s.close();
         sf.close();
 
@@ -43,10 +68,7 @@ public class SimpleMainApp {
     public static SessionFactory getSessionFactory() {
         // loads configuration and mappings
         Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
-        ServiceRegistry serviceRegistry
-                = new StandardServiceRegistryBuilder()
-                .applySettings(configuration.getProperties()).build();
-
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
         // builds a session factory from the service registry
         SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         return sessionFactory;
